14ffd07b3eee46483cadb09b4fe069aa234b6f5b
tprabesh86@gmail.com <tprabesh86@gmail.com>
2026-02-25 21:36:08 +0545

    Add setup_logger() and get_logger() for shared honeypot logging

---
+++ honeypot_logger.py
@@ -0,0 +1,36 @@
+import logging
+_logger_instance = None
+
+
+def setup_logger(log_callback=None):
+    global _logger_instance
+    
+    logger = logging.getLogger("HoneypotLogger")
+    logger.setLevel(logging.INFO)
+    
+    if logger.handlers:
+        logger.handlers.clear()
+    
+    ch = logging.StreamHandler()
+    ch.setFormatter(logging.Formatter("%(asctime)s - %(message)s"))
+    logger.addHandler(ch)
+    
+    if log_callback:
+        class GUILogHandler(logging.Handler):
+            def emit(self, record):
+                try:
+                    msg = self.format(record)
+                    log_callback(msg)
+                except Exception:
+                    self.handleError(record)
+        
+        gui_handler = GUILogHandler()
+        gui_handler.setFormatter(logging.Formatter("%(asctime)s - %(message)s"))
+        logger.addHandler(gui_handler)
+    
+    _logger_instance = logger
+    return logger
+
+def get_logger():
+    """Get the shared logger instance."""
+    return _logger_instance
