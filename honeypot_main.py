8f0a16c8f0d145038d7bdff85447d49607ae4288
tprabesh86@gmail.com <tprabesh86@gmail.com>
2026-02-26 20:08:10 +0545

    Connect GUI launch and logging callback in main entry point

--- honeypot_main.py
+++ honeypot_main.py
@@ -11,3 +11,16 @@ class GUICallback:
 
 gui_callback = GUICallback()
 logger = setup_logger(log_callback=gui_callback)
+
+
+def main():
+    from honeypot_gui import launch_gui
+    
+    # Launch the GUI (it will set up gui_callback.gui_ref)
+    launch_gui(gui_callback)
+    
+    logger.info("Application exiting.")
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
