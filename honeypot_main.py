747f354d47e38318131c5651c41bd7dacfbbd757
tprabesh86@gmail.com <tprabesh86@gmail.com>
2026-02-26 20:06:40 +0545

    Add GUICallback class to forward logs to GUI

--- honeypot_main.py
+++ honeypot_main.py
@@ -1,3 +1,13 @@
 import sys
 from honeypot_logger import setup_logger
 
+class GUICallback:
+    def __init__(self):
+        self.gui_ref = None
+    
+    def __call__(self, msg):
+        if self.gui_ref:
+            self.gui_ref.append_log(msg)
+
+gui_callback = GUICallback()
+logger = setup_logger(log_callback=gui_callback)
